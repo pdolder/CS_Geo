656eeb1c8f6c5a7b1d43f43844cdbb5442e38e3f
paul <paul.dolder@gmail.com>
2016-09-27 23:02:38 -0700

    Code for assigning habitats to kmeans knots

--- data/Covariates/AssignHabitat.R
+++ data/Covariates/AssignHabitat.R
@@ -1,14 +1,40 @@
+
+## This can be generalised, but basically takes the Kmeans knots and assigns a
+## habitat based on joining the EMOD data to produce a
+#### dataframe of habitats associated with the kmean centers for a covariate
+
 library(VAST)
+library(rgdal)
 
-## Read in the habitats
-
-load('Habitat_Classifications.RData')
-
-class(file.df.sub)
-
-DF <-  data.frame(X = file.df.sub$long, Y = file.df.sub$lat)
-attr(DF, "projection") = "LL"
-
-DF <- PBSmapping::convUL(DF) 
-
+# Load the kmeans
 load(file.path('..','..','downloads','2016-09-27_RUN2','Kmeans-100.RData'))
+
+# Create dataframe of kmeans and convert back to LLs
+DF <- data.frame(X = Kmeans$centers[,'E_km'], Y = Kmeans$centers[,'N_km'])
+attr(DF, 'projection') = 'UTM'
+attr(DF, "zone") = 30 
+LLs <- PBSmapping::convUL(DF)
+
+# Distances between sample points
+hist(dist(coordinates(LLs)))
+
+## http://rstudio-pubs-static.s3.amazonaws.com/7993_6b081819ba184047802a508a7f3187cb.html
+
+# Read in frame
+HabMap <- readOGR(file.path('201208_EUSeaMap_Atlantic_Habitats'),'201208_EUSeaMap_Atlantic_Habitats', verbose = F)
+
+## Join on the spatial points
+
+LLs <- SpatialPoints(LLs)
+proj4string(LLs) <- CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0')
+
+join <- over(LLs, HabMap)
+
+LLs <- SpatialPointsDataFrame(LLs, join)
+head(LLs)
+
+table(LLs$Level2_des)
+
+KmeanHab <- data.frame(Habitat = LLs$Level2_des)
+
+save(KmeanHab, file = 'KmeansHab.RData')
